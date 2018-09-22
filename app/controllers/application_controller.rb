1693cdad19f3a95da7d53931ed2e582ba33d27a2
Cedric David <cedric.david@btopenworld.com>
2018-09-22 01:56:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+    
+      erb :error
+    end
   end
 
   get '/account' do
