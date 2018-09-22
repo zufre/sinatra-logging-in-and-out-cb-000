95305017f35607b84247a84606e528a6a0f752ef
Cedric David <cedric.david@btopenworld.com>
2018-09-22 02:05:58 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(session)
+    @current_user = User.find_by_id(session[:user_id])
+    @current_user
+  end
+  def self.is_logged_in?(session)
+    !! self.current_user(session)
+  end
 end
